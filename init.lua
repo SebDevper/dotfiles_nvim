a034d661aad44a95a551478516937253a4e74ac7
Abner Sebick <abn.sebick@gmail.com>
2025-03-24 16:38:07 -0300

    se agregan configs a lua

--- init.lua
+++ init.lua
@@ -1,1 +1,53 @@
-print("Hello")
+-- numeros activados
+vim.opt.number = true
+
+-- numeros relativos
+vim.opt.relativenumber = true
+
+-- nuevas ventanas siempre hacia abajo
+vim.opt.splitbelow = true
+
+-- al hacer vsplit siempre hacia al lado
+vim.opt.splitright = true
+
+-- no wrap
+vim.opt.wrap = false
+
+-- solo espacios y no tabs
+vim.opt.expandtab = true
+
+-- cantiadad de espacios que equivale un tab
+vim.opt.tabstop = 2
+
+-- clipboard
+-- El nombre de este clipboard es el que usa internamente vim para conectarse al del sistema operativo
+vim.opt.clipboard = "unnamedplus"
+
+-- el mover el cursor scrollea el documento mientras este en el centro
+vim.opt.scrolloff = 999
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
